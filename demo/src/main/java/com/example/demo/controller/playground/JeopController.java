6c0d4d8a75901b7d622bfcfcffa6eeda72b6f05d
Jesus Emilio Osorio Perez <jeosoriop@ufpso.edu.co>
2025-09-11 16:19:13 -0500

    EPR-4: CRUD jeop

---
+++ demo/src/main/java/com/example/demo/controller/playground/JeopController.java
@@ -0,0 +1,47 @@
+
+package com.example.demo.controller.playground;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("ejemplo/nombres")
+public class JeopController {
+    private List<String> nombres = new ArrayList<>(List.of("Maicol","Jesus","Yademir","Wilder"));
+
+    //Get- Sirve para listar todos los nombre
+    //Enpoint: get/ejemplo/nombres
+    @GetMapping
+    public List<String> getNombres(){
+
+        return nombres;
+    }
+
+    //Post-Agregar nuevo nombre
+    //Endpoint: Post /ejemplo/nombre?nombre=jhonatan
+    @PostMapping
+    public String addNombres(@RequestParam String nombre){
+        nombres.add(nombre);
+        System.out.println("nombres added");
+        return "nombre agregado"+nombre;
+    }
+
+    //PUT-Actualizar un nombre existente
+    //Enpoint: Put /ejemplo/nombres/3?nuevo= nombre nuevo
+    @PutMapping("/{index}")
+    public String updateNombres(@PathVariable int index, @RequestParam String nuevo){
+        nombres.set(index, nuevo);
+        return "nombre actualizado"+index+"es: "+nuevo;
+    }
+
+    //DELETE - Eliminar nombre
+    //Enpoint: DELETE /ejemplo/nombres/1
+    @DeleteMapping("/{index}")
+    public String deleteNombres(@PathVariable int index){
+        String eliminado=nombres.remove(index);
+        return "nombre eliminado"+index+"es: "+eliminado;
+    }
+
+}
