795b65cb23d6243967a71b63be1a9300f096fbd8
Jesus Emilio Osorio Perez <jeosoriop@ufpso.edu.co>
2025-10-10 15:32:26 -0500

    EPR-20: creacion y configuracion de businesstype

--- demo/src/main/java/com/example/demo/repository/BusinessTypeRepository.java
+++ demo/src/main/java/com/example/demo/repository/BusinessTypeRepository.java
@@ -1,4 +1,4 @@
 package com.example.demo.repository;
 import com.example.demo.model.BusinessType;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface BusinessTypeRepository extends JpaRepository<BusinessType, Integer> { }
+public interface BusinessTypeRepository extends JpaRepository<BusinessType, Long> { }
